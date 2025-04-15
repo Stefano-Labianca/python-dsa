7b8168a8d89807973241fa9a00401ff03d98b142
Stefano Labianca <lstefano@proton.me>
2025-04-15 16:20:13 +0200

    added Insertion Sort

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+from algorithms.sorting.insertion_sort import insertion_sort
 from algorithms.sorting.merge_sort import merge_sort
 from algorithms.sorting.bubble_sort import bubble_sort
 
 arr = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 
 print(
-    f"Bubble Sort: {bubble_sort(arr)}", f"Merge Sort: {merge_sort(arr)}", 
+    f"Bubble Sort: {bubble_sort(arr)}", f"Merge Sort: {merge_sort(arr)}",
+    f"Insertion Sort: {insertion_sort(arr)}" ,
     sep="\n"
 )
 
