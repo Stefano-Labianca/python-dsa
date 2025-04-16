fd8f1aca32c291030ad75cf547d7e436c65eecbc
Stefano Labianca <lstefano@proton.me>
2025-04-16 17:45:06 +0200

    added Selection Sort

---
+++ algorithms/sorting/selection_sort.py
@@ -0,0 +1,9 @@
+def selection_sort(nums: list[int]):
+    for i in range(len(nums)):
+        smallest_idx = i
+
+        for j in range(smallest_idx + 1, len(nums)):
+            if nums[j] < nums[smallest_idx]:
+                smallest_idx = j
+
+        nums[i], nums[smallest_idx] = nums[smallest_idx], nums[i]
--- main.py
+++ main.py
@@ -2,16 +2,14 @@
 # from algorithms.sorting.merge_sort import merge_sort
 # from algorithms.sorting.bubble_sort import bubble_sort
 
-from algorithms.sorting.quick_sort import quick_sort
+from algorithms.sorting.selection_sort import selection_sort
 
 arr = [9, 6, 2, 1, 8, 7]
 
-# print(
-#     f"Bubble Sort: {bubble_sort(arr)}", f"Merge Sort: {merge_sort(arr)}",
-#     f"Insertion Sort: {insertion_sort(arr)}" ,
-#     sep="\n"
-# )
+# bubble_sort(arr)
+# insertion_sort(arr)
+selection_sort(arr)
+# quick_sort(arr, 0, len(arr) - 1)
 
 
-quick_sort(arr, 0, len(arr) - 1)
 print(arr)
