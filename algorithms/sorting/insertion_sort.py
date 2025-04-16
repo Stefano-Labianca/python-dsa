6e2cd890036f420115e3c53f71e0cfd0e54b23b6
Stefano Labianca <lstefano@proton.me>
2025-04-16 16:31:06 +0200

    Insertion Sort now don't return a copy of the sorted array

--- algorithms/sorting/insertion_sort.py
+++ algorithms/sorting/insertion_sort.py
@@ -1,9 +1,7 @@
-def insertion_sort(nums: list[int]) -> list[int]:
+def insertion_sort(nums: list[int]):
     for i in range(len(nums)):
         j = i
         
         while j > 0 and nums[j - 1] > nums[j]:
             nums[j - 1], nums[j] = nums[j], nums[j - 1]
             j -= 1
-            
-    return nums
