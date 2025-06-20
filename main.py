c28ba0e539da31679f671531ee3449392f36daa7
Stefano Labianca <lstefano@proton.me>
2025-06-20 17:56:20 +0200

    test cases

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 from data_structures.graph import Graph
 
 graph = Graph()
-graph.add_edge(0, 1)
-graph.add_edge(0, 2)
-graph.add_edge(1, 3)
-graph.add_edge(2, 3)
 
-print(graph)
+graph.add_edge("New York", "London")
+graph.add_edge("New York", "Cairo")
+graph.add_edge("New York", "Tokyo")
+graph.add_edge("London", "Dubai")
+graph.add_edge("Cairo", "Kyiv")
+graph.add_edge("Cairo", "Madrid")
+graph.add_edge("London", "Madrid")
+graph.add_edge("Buenos Aires", "New York")
+graph.add_edge("Tokyo", "Buenos Aires")
+graph.add_edge("Kyiv", "San Francisco")
+
+print(graph.breadth_first_search("New York"))
